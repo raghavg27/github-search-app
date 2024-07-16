df423476fc6b1f963b32f434664e28a1e7eea744
raghavg27 <raghavg2705@gmail.com>
2024-07-16 16:20:18 +0530

    api error

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,7 +14,7 @@ import { debounce } from './utils';
 import axios from 'axios';
 
 const API_URL = 'https://github-search-app.onrender.com/api/users';
-const GITHUB_TOKEN = process.env.REACT_APP_API_KEY;
+const GITHUB_TOKEN = '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';
 
 function App() {
   const [mode, setMode] = useState('light');
